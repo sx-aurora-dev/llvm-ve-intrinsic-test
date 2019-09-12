a9be27be21d91ab4f65645b0ab45d28d0ba6811c
Kazuhisa Ishizaka <k-ishizaka@ay.jp.nec.com>
2019-09-12 12:42:49 +0900

    Add vel_vst*2d_vssml

--- tests/vel_vldvst2d.cc
+++ tests/vel_vldvst2d.cc
@@ -14,6 +14,15 @@ void vel_vst2d(double* px, unsigned long int sy, void* sz)
     _vel_vst2d_vssl(vx, sy, sz, 256);
 }
 
+void vel_vst2d_vssml(double* px, unsigned long int sy, void* sz, unsigned int* pm)
+{
+    ;
+    __vr vx = _vel_vld_vssl(8, px, 256);
+    __vr tmp = _vel_vldlsx_vssl(4, pm, 256);
+    __vm vm = _vel_vfmkwgt_mvl(tmp, 256);
+    _vel_vst2d_vssml(vx, sy, sz, vm, 256);
+}
+
 // upper
 void vel_vldu2d(float* px, unsigned long int sy, void const* sz)
 {
@@ -27,6 +36,15 @@ void vel_vstu2d(float* px, unsigned long int sy, void* sz)
     ;
     __vr vx = _vel_vldu_vssl(4, px, 256);
     _vel_vstu2d_vssl(vx, sy, sz, 256);
+}
+
+void vel_vstu2d_vssml(float* px, unsigned long int sy, void* sz, unsigned int* pm)
+{
+    ;
+    __vr vx = _vel_vldu_vssl(4, px, 256);
+    __vr tmp = _vel_vldlsx_vssl(4, pm, 256);
+    __vm vm = _vel_vfmkwgt_mvl(tmp, 256);
+    _vel_vstu2d_vssml(vx, sy, sz, vm, 256);
 }
 
 // lower
@@ -49,6 +67,15 @@ void vel_vstl2d(int* px, unsigned long int sy, void* sz)
     ;
     __vr vx = _vel_vldlsx_vssl(4, px, 256);
     _vel_vstl2d_vssl(vx, sy, sz, 256);
+}
+
+void vel_vstl2d_vssml(int* px, unsigned long int sy, void* sz, unsigned int* pm)
+{
+    ;
+    __vr vx = _vel_vldlsx_vssl(4, px, 256);
+    __vr tmp = _vel_vldlsx_vssl(4, pm, 256);
+    __vm vm = _vel_vfmkwgt_mvl(tmp, 256);
+    _vel_vstl2d_vssml(vx, sy, sz, vm, 256);
 }
 #endif
 
@@ -85,7 +112,7 @@ int test_vel_vld2d_tmpl(void (*f)(T1*, unsigned long int, void const*))
     int flag = 1;
     for (int i = 0; i < 256; ++i) {
 #ifdef MAIN
-        fprintf(stderr, fmt<T1>::s, vx[i], i%8 == 7 ? "\n": "");
+        fprintf(stderr, fmt<T1>::s, vx[i], i%16 == 15 ? "\n": "");
 #endif
         flag &= vx[i] == A[STR*(i/16)+STC*(i%16)];
     }
@@ -117,9 +144,73 @@ int test_vel_vst2d_tmpl(void (*f)(T*, unsigned long int, void*))
     int flag = 1;
     for (int i = 0; i < 256; ++i) {
 #ifdef MAIN
-        fprintf(stderr, fmt<T>::s, A[STR*(i/16)+STC*(i%16)], i%8 == 7 ? "\n": "");
+        fprintf(stderr, fmt<T>::s, A[STR*(i/16)+STC*(i%16)], i%16 == 15 ? "\n": "");
 #endif
         flag &= vx[i] == A[STR*(i/16)+STC*(i%16)];
+    }
+
+#ifdef MAIN
+    fprintf(stderr, "%s: %s\n", __PRETTY_FUNCTION__, flag ? "OK" : "NG");
+#endif
+
+    return flag;
+}
+
+template <typename T>
+int test_vel_vst2d_mask_tmpl(void (*f)(T*, unsigned long int, void*, unsigned int*))
+{
+    T A[16*256];
+    T vx[256];
+    unsigned int mask[256];
+
+    memset(A, 0, sizeof(A));
+    for (int i = 0; i < 256; ++i) {
+        vx[i] = getrand<T>();
+        mask[i] = getrand<unsigned int>() % 2;
+    }
+
+    int STR = 256;
+    int STC = 1;
+
+    unsigned long int sy = (STR*sizeof(T)) << 16 | (STC*sizeof(T));
+
+    f(vx, sy, A, mask);
+
+#if 0
+    for (int i = 0; i < 256; ++i) {
+        fprintf(stderr, " %8u", mask[i]);
+        if (i % 16 == 15)
+            fprintf(stderr, "\n");
+    }
+
+    for (int i = 0; i < 256; ++i) {
+        fprintf(stderr, " %8.3f", vx[i]);
+        if (i % 16 == 15)
+            fprintf(stderr, "\n");
+    }
+
+    for (int j = 0; j < 16; ++j) {
+        fprintf(stderr, "%02d: ", j);
+        for (int i = 0; i < 256; ++i) {
+            fprintf(stderr, " %8.3f", A[j*256+i]);
+            if (i % 16 == 15) {
+                fprintf(stderr, "\n");
+                if (i != 255)
+                    fprintf(stderr, "%02d: ", j);
+            }
+        }
+    }
+#endif
+
+    int flag = 1;
+    for (int i = 0; i < 256; ++i) {
+#ifdef MAIN
+        fprintf(stderr, fmt<T>::s, A[STR*(i/16)+STC*(i%16)], i%16 == 15 ? "\n": "");
+#endif
+        if (mask[i])
+            flag &= vx[i] == A[STR*(i/16)+STC*(i%16)];
+        else
+            flag &= A[STR*(i/16)+STC*(i%16)] == 0;
     }
 
 #ifdef MAIN
@@ -171,6 +262,24 @@ int test_vel_vstl2d()
     return test_vel_vst2d_tmpl(vel_vstl2d);
 }
 
+int test_vel_vst2d_vssml()
+{
+    void vel_vst2d_vssml(double* px, unsigned long int sy, void* sz, unsigned int* pm);
+    return test_vel_vst2d_mask_tmpl(vel_vst2d_vssml);
+}
+
+int test_vel_vstu2d_vssml()
+{
+    void vel_vstu2d_vssml(float* px, unsigned long int sy, void* sz, unsigned int* pm);
+    return test_vel_vst2d_mask_tmpl(vel_vstu2d_vssml);
+}
+
+int test_vel_vstl2d_vssml()
+{
+    void vel_vstl2d_vssml(int* px, unsigned long int sy, void* sz, unsigned int* pm);
+    return test_vel_vst2d_mask_tmpl(vel_vstl2d_vssml);
+}
+
 #ifdef HAVE_REGISTER_TEST
 #include "register_test.h"
 REGISTER_TEST("vel_vld2d", test_vel_vld2d)
@@ -180,6 +289,9 @@ REGISTER_TEST("vel_vldl2dzx", test_vel_vldl2dzx)
 REGISTER_TEST("vel_vst2d", test_vel_vst2d)
 REGISTER_TEST("vel_vstu2d", test_vel_vstu2d)
 REGISTER_TEST("vel_vstl2d", test_vel_vstl2d)
+REGISTER_TEST("vel_vst2d_vssml", test_vel_vst2d_vssml)
+REGISTER_TEST("vel_vstu2d_vssml", test_vel_vstu2d_vssml)
+REGISTER_TEST("vel_vstl2d_vssml", test_vel_vstl2d_vssml)
 #endif
 #endif // TEST
 
@@ -195,6 +307,9 @@ int main(int argc, char* argv[])
         && test_vel_vst2d()
         && test_vel_vstu2d()
         && test_vel_vstl2d()
+        && test_vel_vst2d_vssml()
+        && test_vel_vstu2d_vssml()
+        && test_vel_vstl2d_vssml()
         ? 0 : 1;
 }
 #endif
