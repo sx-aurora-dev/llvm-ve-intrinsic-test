5e6e5de091696be8089b6dcf0f4b737fed07be75
Kazuhisa Ishizaka <k-ishizaka@ay.jp.nec.com>
2019-09-24 09:37:11 +0900

    Add tests for vst*.nc.ot

--- asm/tests/vel_vstot.s
+++ asm/tests/vel_vstot.s
@@ -114,6 +114,120 @@ _Z14vel_vstlot_vssPKiPiS1_:
 .Lfunc_end2:
 	.size	_Z14vel_vstlot_vssPKiPiS1_, .Lfunc_end2-_Z14vel_vstlot_vssPKiPiS1_
 
+	.globl	_Z15vel_vstncot_vssPKdPdS1_
+	.p2align	4
+	.type	_Z15vel_vstncot_vssPKdPdS1_,@function
+_Z15vel_vstncot_vssPKdPdS1_:
+	st %s9, (,%s11)
+	st %s10, 8(,%s11)
+	st %s15, 24(,%s11)
+	st %s16, 32(,%s11)
+	or %s9, 0, %s11
+	lea %s13, -176
+	and %s13, %s13, (32)0
+	lea.sl %s11, -1(%s11, %s13)
+	brge.l %s11, %s8, .LBB3_2
+	ld %s61, 24(,%s14)
+	or %s62, 0, %s0
+	lea %s63, 315
+	shm.l %s63, (%s61)
+	shm.l %s8, 8(%s61)
+	shm.l %s11, 16(%s61)
+	monc
+	or %s0, 0, %s62
+.LBB3_2:
+	lea %s34, 256
+	lvl %s34
+	vld %v0,8,%s0
+	vst.nc.ot %v0,8,%s2
+	svob
+	vld %v0,8,%s2
+	vst %v0,8,%s1
+	or %s11, 0, %s9
+	ld %s16, 32(,%s11)
+	ld %s15, 24(,%s11)
+	ld %s10, 8(,%s11)
+	ld %s9, (,%s11)
+	b.l (,%lr)
+.Lfunc_end3:
+	.size	_Z15vel_vstncot_vssPKdPdS1_, .Lfunc_end3-_Z15vel_vstncot_vssPKdPdS1_
 
-	.ident	"clang version 9.0.0 (git@socsv218.svp.cl.nec.co.jp:ve-llvm/clang.git d467e4f0338396acbc980449a89d05f037ef4a6c) (llvm/llvm.git d49dca7a0a80fc0a3ee8f0716752ae072548b21c)"
+	.globl	_Z16vel_vstuncot_vssPKfPfS1_
+	.p2align	4
+	.type	_Z16vel_vstuncot_vssPKfPfS1_,@function
+_Z16vel_vstuncot_vssPKfPfS1_:
+	st %s9, (,%s11)
+	st %s10, 8(,%s11)
+	st %s15, 24(,%s11)
+	st %s16, 32(,%s11)
+	or %s9, 0, %s11
+	lea %s13, -176
+	and %s13, %s13, (32)0
+	lea.sl %s11, -1(%s11, %s13)
+	brge.l %s11, %s8, .LBB4_2
+	ld %s61, 24(,%s14)
+	or %s62, 0, %s0
+	lea %s63, 315
+	shm.l %s63, (%s61)
+	shm.l %s8, 8(%s61)
+	shm.l %s11, 16(%s61)
+	monc
+	or %s0, 0, %s62
+.LBB4_2:
+	lea %s34, 256
+	lvl %s34
+	vldu %v0,4,%s0
+	vstu.nc.ot %v0,4,%s2
+	svob
+	vldu %v0,4,%s2
+	vstu %v0,4,%s1
+	or %s11, 0, %s9
+	ld %s16, 32(,%s11)
+	ld %s15, 24(,%s11)
+	ld %s10, 8(,%s11)
+	ld %s9, (,%s11)
+	b.l (,%lr)
+.Lfunc_end4:
+	.size	_Z16vel_vstuncot_vssPKfPfS1_, .Lfunc_end4-_Z16vel_vstuncot_vssPKfPfS1_
+
+	.globl	_Z16vel_vstlncot_vssPKiPiS1_
+	.p2align	4
+	.type	_Z16vel_vstlncot_vssPKiPiS1_,@function
+_Z16vel_vstlncot_vssPKiPiS1_:
+	st %s9, (,%s11)
+	st %s10, 8(,%s11)
+	st %s15, 24(,%s11)
+	st %s16, 32(,%s11)
+	or %s9, 0, %s11
+	lea %s13, -176
+	and %s13, %s13, (32)0
+	lea.sl %s11, -1(%s11, %s13)
+	brge.l %s11, %s8, .LBB5_2
+	ld %s61, 24(,%s14)
+	or %s62, 0, %s0
+	lea %s63, 315
+	shm.l %s63, (%s61)
+	shm.l %s8, 8(%s61)
+	shm.l %s11, 16(%s61)
+	monc
+	or %s0, 0, %s62
+.LBB5_2:
+	lea %s34, 256
+	lvl %s34
+	vldl.sx %v0,4,%s0
+	vstl.nc.ot %v0,4,%s2
+	svob
+	vldl.sx %v0,4,%s2
+	vstl %v0,4,%s1
+	or %s11, 0, %s9
+	ld %s16, 32(,%s11)
+	ld %s15, 24(,%s11)
+	ld %s10, 8(,%s11)
+	ld %s9, (,%s11)
+	b.l (,%lr)
+.Lfunc_end5:
+	.size	_Z16vel_vstlncot_vssPKiPiS1_, .Lfunc_end5-_Z16vel_vstlncot_vssPKiPiS1_
+
+
+	.ident	"clang version 10.0.0 (git@socsv218.svp.cl.nec.co.jp:ve-llvm/llvm-project.git 684f35c67e2d7e0a474732547d535e7efd3486cf)"
 	.section	".note.GNU-stack","",@progbits
--- tests/vel_vstot.cc
+++ tests/vel_vstot.cc
@@ -2,7 +2,6 @@
 #include <velintrin.h>
 void vel_vstot_vss(double const* px, double* py, double* ptmp)
 {
-    ;
     __vr vx = _vel_vld_vssl(8, px, 256);
     _vel_vstot_vssl(vx, 8, ptmp, 256);
     _vel_svob();
@@ -12,7 +11,6 @@ void vel_vstot_vss(double const* px, double* py, double* ptmp)
 
 void vel_vstuot_vss(float const* px, float* py, float* ptmp)
 {
-    ;
     __vr vx = _vel_vldu_vssl(4, px, 256);
     _vel_vstuot_vssl(vx, 4, ptmp, 256);
     _vel_svob();
@@ -22,9 +20,37 @@ void vel_vstuot_vss(float const* px, float* py, float* ptmp)
 
 void vel_vstlot_vss(int const* px, int* py, int* ptmp)
 {
-    ;
     __vr vx = _vel_vldlsx_vssl(4, px, 256);
     _vel_vstlot_vssl(vx, 4, ptmp, 256);
+    _vel_svob();
+    __vr vy = _vel_vldlsx_vssl(4, ptmp, 256);
+    _vel_vstl_vssl(vy, 4, py, 256);
+}
+
+// .nc
+
+void vel_vstncot_vss(double const* px, double* py, double* ptmp)
+{
+    __vr vx = _vel_vld_vssl(8, px, 256);
+    _vel_vstncot_vssl(vx, 8, ptmp, 256);
+    _vel_svob();
+    __vr vy = _vel_vld_vssl(8, ptmp, 256);
+    _vel_vst_vssl(vy, 8, py, 256);
+}
+
+void vel_vstuncot_vss(float const* px, float* py, float* ptmp)
+{
+    __vr vx = _vel_vldu_vssl(4, px, 256);
+    _vel_vstuncot_vssl(vx, 4, ptmp, 256);
+    _vel_svob();
+    __vr vy = _vel_vldu_vssl(4, ptmp, 256);
+    _vel_vstu_vssl(vy, 4, py, 256);
+}
+
+void vel_vstlncot_vss(int const* px, int* py, int* ptmp)
+{
+    __vr vx = _vel_vldlsx_vssl(4, px, 256);
+    _vel_vstlncot_vssl(vx, 4, ptmp, 256);
     _vel_svob();
     __vr vy = _vel_vldlsx_vssl(4, ptmp, 256);
     _vel_vstl_vssl(vy, 4, py, 256);
@@ -72,11 +98,19 @@ DEFINE_TEST(vel_vstot_vss, double);
 DEFINE_TEST(vel_vstuot_vss, float);
 DEFINE_TEST(vel_vstlot_vss, int);
 
+DEFINE_TEST(vel_vstncot_vss, double);
+DEFINE_TEST(vel_vstuncot_vss, float);
+DEFINE_TEST(vel_vstlncot_vss, int);
+
 #ifdef HAVE_REGISTER_TEST
 #include "register_test.h"
 REGISTER_TEST("vel_vstot_vss", test_vel_vstot_vss);
 REGISTER_TEST("vel_vstuot_vss", test_vel_vstuot_vss);
 REGISTER_TEST("vel_vstlot_vss", test_vel_vstlot_vss);
+
+REGISTER_TEST("vel_vstncot_vss", test_vel_vstncot_vss);
+REGISTER_TEST("vel_vstuncot_vss", test_vel_vstuncot_vss);
+REGISTER_TEST("vel_vstlncot_vss", test_vel_vstlncot_vss);
 #endif
 #endif // TEST
 
@@ -87,6 +121,10 @@ int main(int argc, char* argv[])
     fprintf(stderr, "test_vel_vstot_vss: %s\n", test_vel_vstot_vss() ? "OK" : "NG");
     fprintf(stderr, "test_vel_vstuot_vss: %s\n", test_vel_vstuot_vss() ? "OK" : "NG");
     fprintf(stderr, "test_vel_vstlot_vss: %s\n", test_vel_vstlot_vss() ? "OK" : "NG");
+
+    fprintf(stderr, "test_vel_vstncot_vss: %s\n", test_vel_vstncot_vss() ? "OK" : "NG");
+    fprintf(stderr, "test_vel_vstuncot_vss: %s\n", test_vel_vstuncot_vss() ? "OK" : "NG");
+    fprintf(stderr, "test_vel_vstlncot_vss: %s\n", test_vel_vstlncot_vss() ? "OK" : "NG");
     return 0;
 }
 #endif
