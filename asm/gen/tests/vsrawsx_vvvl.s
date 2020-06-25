601fa9e7e934a6f5b945f882297c0d9552de1ee6
Kazuhisa Ishizaka <ishizaka@nec.com>
2020-06-25 19:59:36 +0900

    Add asm

---
+++ asm/gen/tests/vsrawsx_vvvl.s
@@ -0,0 +1,53 @@
+	.text
+	.file	"vsrawsx_vvvl.c"
+	.globl	vsrawsx_vvvl
+	.p2align	4
+	.type	vsrawsx_vvvl,@function
+vsrawsx_vvvl:
+	st %s9, (, %s11)
+	st %s10, 8(, %s11)
+	st %s15, 24(, %s11)
+	st %s16, 32(, %s11)
+	or %s9, 0, %s11
+	lea %s13, -176
+	and %s13, %s13, (32)0
+	lea.sl %s11, -1(%s13, %s11)
+	brge.l %s11, %s8, .LBB0_5
+	ld %s61, 24(, %s14)
+	or %s62, 0, %s0
+	lea %s63, 315
+	shm.l %s63, (%s61)
+	shm.l %s8, 8(%s61)
+	shm.l %s11, 16(%s61)
+	monc
+	or %s0, 0, %s62
+.LBB0_5:
+	brgt.w 1, %s3, .LBB0_3
+	or %s4, 0, (0)1
+	lea %s5, 256
+	or %s6, 0, %s3
+.LBB0_2:
+	mins.w.sx %s7, %s6, %s5
+	lvl %s7
+	vldl.sx %v0,4,%s1
+	vld %v1,8,%s2
+	vsra.w.sx %v0,%v0,%v1
+	vstl %v0,4,%s0
+	lea %s0, 1024(, %s0)
+	lea %s1, 1024(, %s1)
+	lea %s2, 2048(, %s2)
+	adds.w.sx %s4, %s4, %s5
+	adds.w.sx %s6, %s6, (56)1
+	brlt.w %s4, %s3, .LBB0_2
+.LBB0_3:
+	or %s11, 0, %s9
+	ld %s16, 32(, %s11)
+	ld %s15, 24(, %s11)
+	ld %s10, 8(, %s11)
+	ld %s9, (, %s11)
+	b.l.t (, %s10)
+.Lfunc_end0:
+	.size	vsrawsx_vvvl, .Lfunc_end0-vsrawsx_vvvl
+
+	.ident	"clang version 11.0.0 (git@socsv218.svp.cl.nec.co.jp:ve-llvm/llvm-project.git 6494a5b9576f8e8af5147e966098a7e8a3dad78f)"
+	.section	".note.GNU-stack","",@progbits
---
+++ asm/gen/tests/vsrawzx_vvsmvl_imm.s
@@ -0,0 +1,56 @@
+	.text
+	.file	"vsrawzx_vvsmvl_imm.c"
+	.globl	vsrawzx_vvsmvl_imm
+	.p2align	4
+	.type	vsrawzx_vvsmvl_imm,@function
+vsrawzx_vvsmvl_imm:
+	st %s9, (, %s11)
+	st %s10, 8(, %s11)
+	st %s15, 24(, %s11)
+	st %s16, 32(, %s11)
+	or %s9, 0, %s11
+	lea %s13, -176
+	and %s13, %s13, (32)0
+	lea.sl %s11, -1(%s13, %s11)
+	brge.l %s11, %s8, .LBB0_5
+	ld %s61, 24(, %s14)
+	or %s62, 0, %s0
+	lea %s63, 315
+	shm.l %s63, (%s61)
+	shm.l %s8, 8(%s61)
+	shm.l %s11, 16(%s61)
+	monc
+	or %s0, 0, %s62
+.LBB0_5:
+	brgt.w 1, %s4, .LBB0_3
+	or %s5, 0, (0)1
+	lea %s6, 256
+	or %s7, 0, %s4
+.LBB0_2:
+	mins.w.sx %s34, %s7, %s6
+	lvl %s34
+	vldl.sx %v0,4,%s1
+	vldl.zx %v1,4,%s2
+	vldl.sx %v2,4,%s3
+	vfmk.w.gt %vm1,%v1
+	vsra.w.zx %v2,%v0,3,%vm1
+	vstl %v2,4,%s0
+	lea %s0, 1024(, %s0)
+	lea %s1, 1024(, %s1)
+	lea %s2, 1024(, %s2)
+	lea %s3, 1024(, %s3)
+	adds.w.sx %s5, %s5, %s6
+	adds.w.sx %s7, %s7, (56)1
+	brlt.w %s5, %s4, .LBB0_2
+.LBB0_3:
+	or %s11, 0, %s9
+	ld %s16, 32(, %s11)
+	ld %s15, 24(, %s11)
+	ld %s10, 8(, %s11)
+	ld %s9, (, %s11)
+	b.l.t (, %s10)
+.Lfunc_end0:
+	.size	vsrawzx_vvsmvl_imm, .Lfunc_end0-vsrawzx_vvsmvl_imm
+
+	.ident	"clang version 11.0.0 (git@socsv218.svp.cl.nec.co.jp:ve-llvm/llvm-project.git 6494a5b9576f8e8af5147e966098a7e8a3dad78f)"
+	.section	".note.GNU-stack","",@progbits
